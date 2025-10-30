c4fe3f4440ec640d5ffdc21563cdb0eb12573e6b
MMAGES2006 <22170@portalcedi.edu.mx>
2025-10-30 11:58:57 -0600

    Menu 1

---
+++ Menu.hpp
@@ -0,0 +1,30 @@
+#ifndef Menu_h
+#define Menu_h
+#include <SFML/Graphics.hpp>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+using namespace sf;
+
+#define Max_menu 4
+
+class Menu
+{
+	int menuSelected;
+	Font font;
+	Text mainMenu[Max_menu];	
+
+public:
+	Menu(float alto, float ancho);
+
+	void dibujar(RenderWindow& window);
+	void moveUp(); 
+	void moveDown();
+	
+	int optSelected();
+
+	~Menu();
+};
+
+#endif
