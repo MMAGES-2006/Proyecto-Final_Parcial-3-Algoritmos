6e102dbd1d02cd66ac83bbaa3e3ce9add9ab39d3
MMAGES2006 <22170@portalcedi.edu.mx>
2025-11-04 10:05:39 -0600

    Correccione

--- main.cpp
+++ main.cpp
@@ -10,5 +10,16 @@ using namespace sf;
 int main()
 {
 
+	Renderwindow mainMENU(VideoMode(960, 720), "Menu", Style::Default);
+	Menu Menu(mainMENU.getSize().x, mainMENU.getSize().y);
+	RectangleShape fondo;
+	fondo.setSize(vector2f(960, 720));
+	Texture UpParking;
+	if (!UpParking.loadFromFile())
+	{
+		cout << "File not founded :(";
+	}
+	fondo.setTexture(&UpParking);
+
 
 }
